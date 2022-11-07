3dcb4607182b968d20008dcc8476cefa8043b7c3
Tahzeeb - windows <ishmum20000@gmail.com>
2022-11-07 15:17:37 +0800

    Update nav bar

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <Navbar bg='danger' variant='dark'>
+            <Navbar bg='danger' variant='dark' style={{height:"70px"}}>
                 <Container>
                     <Nav>
                         <Navbar.Brand as={Link} to="/home">JomTapau</Navbar.Brand>
-                        <Nav.Link className='text-white' as={Link} to="/home">Menu</Nav.Link>
+                        <Nav.Link className='text-white' as={Link} to="/menu">Menu</Nav.Link>
                         <Nav.Link className='text-white' as={Link} to="/data">About</Nav.Link>
                     </Nav>
                     <Nav className=''>
