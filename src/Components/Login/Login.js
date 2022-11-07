393018dc883c8b13a5f1c2e4e8bc455e660ad94e
adnanshafi99 <adnanshafi@graduate.utm.my>
2022-11-07 21:08:27 +0800

    Add background image

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div >
-           
+        <div>
+            
+           <section className=' w-lg-50'>
+          <img
+            className=' w-100 h-100'
+            src='https://i.ibb.co/Dg3F3FV/IMG-9325.jpg'
+            alt=''
+          />
+        </section>
+
         </div>
     );
 };
