87427e4c7123f2c600c5dfd427c0284f0e99a655
Tahzeeb - windows <ishmum20000@gmail.com>
2022-11-07 12:02:53 +0800

    Add nav bar

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const header = () => {
+const Header = () => {
     return (
-        <div className='header'>
-            
+        <div>
+            <Navbar bg='danger' variant='dark'>
+                <Container>
+                    <Nav>
+                        <Navbar.Brand as={Link} to="/home">JomTapau</Navbar.Brand>
+                        <Nav.Link className='text-white' as={Link} to="/home">Menu</Nav.Link>
+                        <Nav.Link className='text-white' as={Link} to="/data">About</Nav.Link>
+                    </Nav>
+                    <Nav className=''>
+                        <Nav.Link className='text-white' as={Link} to="/login">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
 
-export default header;
+export default Header;
