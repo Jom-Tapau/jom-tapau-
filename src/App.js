414a43f869d8d3c0401b4b7c9c3184d46ad48eec
Tahzeeb - windows <ishmum20000@gmail.com>
2022-11-07 15:12:19 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/menu" element={<Menu></Menu>}></Route>
+      </Routes>
     </div>
   );
 }
