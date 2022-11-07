a7a90bd196d4e5912eef39098ee50e7357fd4aa0
adnanshafi99 <adnanshafi@graduate.utm.my>
2022-11-07 23:09:08 +0800

    Add login form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Login = () => {
     return (
         <div>
-            
+
            <section className=' w-lg-50'>
           <img
             className=' w-100 h-100'
@@ -11,6 +12,49 @@ const Login = () => {
             alt=''
           />
         </section>
+        <section className='w-75 mx-auto px-lg-5'>
+          <h1 style={{fontSize:'80px'}} className='waviy text-center mt-5 mb-2 fst-italic'>
+            <span style={{'--i':1}} className='shadow-red text-white'>J</span>
+            <span style={{'--i':2}} className='shadow-white text-danger'>om</span>
+            <small></small>
+            <span style={{'--i':3}} className='shadow-red text-white'>Ta</span>
+            <span style={{'--i':4}} className='shadow-white text-danger'>pa</span>
+            <span style={{'--i':5}} className='shadow-red text-white'>u</span>
+          </h1>
+          <div style={{'--i':6}} className='d-flex justify-content-center'>
+            <div>
+            <p className='text-center mt-5 fs-3'>Log In</p>
+            <hr style={{width:'400px',color:'green',border:'2px solid green'}}/>
+            </div>
+          </div>
+          <form className='w-100 '>
+            <div className='login-container'>
+              <div className='did-floating-label-content'>
+                <input className='did-floating-input' type='email' placeholder=' ' />
+                <label className='did-floating-label'>Email</label>
+              </div>
+              <div className='did-floating-label-content did-error-input'>
+                <input className='did-floating-input' type='password' placeholder=' ' />
+                <label className='did-floating-label'>Password</label>
+              </div>
+            </div>
+            <div className='d-flex  justify-content-center'>
+              <Button className='mb-5' variant='danger' type='submit'>
+                Log In
+              </Button>
+            </div>
+          </form>
+          <div className='d-flex justify-content-center '>
+            <hr style={{width:'200px',color:'red'}}/>
+            <p className='px-3 pt-1'>or</p>
+            <hr style={{width:'200px',color:'red'}}/>
+          </div>
+          <div className='d-flex justify-content-center'>
+            <Button style={{width:'200px'}}  variant="success">Google Sign</Button>
+          </div>
+        </section>
+        
+
 
         </div>
     );
