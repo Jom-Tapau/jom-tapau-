36356fdd092abaa20d733010b12c47a6eddd4883
adnanshafi99 <adnanshafi@graduate.utm.my>
2022-11-09 01:30:29 +0800

    Adding registration structure

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -4,9 +4,72 @@ import { Button } from 'react-bootstrap';
 const Registration = () => {
     return (
         <div>
-            
-          <h1>Test</h1>
 
+        <div className='d-flex justify-content-center'>
+            <div>
+              <p className='text-center mt-5 fs-3'>
+                Fill out the Form to login
+              </p>
+              <hr
+                style={{
+                  width: '400px',
+                  color: 'green',
+                  border: '2px solid green'
+                }}
+              />
+            </div>
+        </div>
+        <form className='w-100 '>
+            <div className='login-container'>
+              <div className='did-floating-label-content'>
+                <input
+                  className='did-floating-input'
+                  type='text'
+                  placeholder=' '
+                />
+                <label className='did-floating-label'>Name</label>
+              </div>
+              <div className='did-floating-label-content'>
+                <input
+                  className='did-floating-input'
+                  type='email'
+                  placeholder=' '
+                />
+                <label className='did-floating-label'>Email</label>
+              </div>
+              <div className='did-floating-label-content'>
+                <input
+                  className='did-floating-input'
+                  type='text'
+                  placeholder=' '
+                />
+                <label className='did-floating-label'>Phone Number</label>
+              </div>
+              <div className='did-floating-label-content did-error-input'>
+                <input
+                  className='did-floating-input'
+                  type='password'
+                  placeholder=' '
+                />
+                <label className='did-floating-label'>Password</label>
+              </div>
+              <div className='did-floating-label-content did-error-input'>
+                <input
+                  className='did-floating-input'
+                  type='password'
+                  placeholder=' '
+                />
+                <label className='did-floating-label'>Confirm Password</label>
+              </div>
+            </div>
+            <div className='d-flex  justify-content-center'>
+              <Button className='mb-5' variant='danger' type='submit'>
+                Sign up
+              </Button>
+            </div>
+          </form>
+
+            
         </div>
     );
 };
