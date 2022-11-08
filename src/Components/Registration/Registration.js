a0d115b19b6a1c8e796d5bb71d4ae6cfcde7b350
adnanshafi99 <adnanshafi@graduate.utm.my>
2022-11-09 00:20:08 +0800

    Add Registration Route

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Registration = () => {
     return (
         <div>
             
+          <h1>Test</h1>
+
         </div>
     );
 };
