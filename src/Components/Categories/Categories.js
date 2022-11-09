9427063531c0e6e060439fa18838ecd26b2835a7
Tahzeeb - windows <ishmum20000@gmail.com>
2022-11-09 20:48:18 +0800

    Add categories

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
 
 const Categories = () => {
     return (
-        <div>
-            
+        <div className='categories'>
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/home">Popular</Nav.Link>
+                <Nav.Link eventKey="link-1">Set Meals</Nav.Link>
+                <Nav.Link eventKey="link-2">A la Carte</Nav.Link>
+                <Nav.Link eventKey="disabled">Nasi</Nav.Link>
+                <Nav.Link eventKey="disabled">Drinks</Nav.Link>
+            </Nav>
         </div>
     );
 };
