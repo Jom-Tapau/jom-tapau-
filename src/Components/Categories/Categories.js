8a71f737b9fe7fc87b48974c2827b9b2d193fe81
Tahzeeb - windows <ishmum20000@gmail.com>
2022-11-09 21:16:13 +0800

    Add class to links

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -6,11 +6,11 @@ const Categories = () => {
     return (
         <div className='categories'>
             <Nav defaultActiveKey="#popular" className="flex-column">
-                <Nav.Link href="#popular">Popular</Nav.Link>
-                <Nav.Link eventKey="link-1">Set Meals</Nav.Link>
-                <Nav.Link eventKey="link-2">A la Carte</Nav.Link>
-                <Nav.Link eventKey="disabled">Nasi</Nav.Link>
-                <Nav.Link eventKey="disabled">Drinks</Nav.Link>
+                <Nav.Link className='link' href="#popular">Popular</Nav.Link>
+                <Nav.Link className='link' eventKey="link-1">Set Meals</Nav.Link>
+                <Nav.Link className='link' eventKey="link-2">A la Carte</Nav.Link>
+                <Nav.Link className='link' eventKey="disabled">Nasi</Nav.Link>
+                <Nav.Link className='link' eventKey="disabled">Drinks</Nav.Link>
             </Nav>
         </div>
     );
