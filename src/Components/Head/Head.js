9613e2148927a44073a79ac87dda669b440695de
GMShimon <simon.rosedale99@gmail.com>
2022-11-12 17:48:59 +0800

    responsive navbar added

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import { useAuthState  } from 'react-firebase-hooks/auth'
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import auth from '../../firebase.init';
-import { signOut } from 'firebase/auth';
-import './Head.css';
-import Loading from '../Loading/Loading';
-
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import auth from '../../firebase.init'
+import { signOut } from 'firebase/auth'
+import './Head.css'
+import Loading from '../Loading/Loading'
 
 const Head = () => {
-    const [user, loading, error] = useAuthState(auth); 
+  const [user, loading, error] = useAuthState(auth)
 
-    if(loading){
-        return <Loading></Loading>
-    }
+  if (loading) {
+    return <Loading></Loading>
+  }
 
-    const handleSignOut = () =>{
-        signOut(auth);
-    }
-    return (
-        <div>
-    
-            <Navbar style={{backgroundColor:'rgb(255,75,75)'}} variant='dark' className='nav-bar'>
-            <Nav><img style={{marginLeft:'100px'}} src='jomlogo.png' width='70' height='70'></img></Nav>
-                <Container>
-                    <Nav className='d-flex align-items-center'>
-                        <Navbar.Brand as={Link} to="/home">
-                        </Navbar.Brand>
-                        <Nav.Link className='text-white' as={Link} to="/menu">Menu</Nav.Link>
-                        <Nav.Link className='text-white' as={Link} to="/about">About</Nav.Link>
-                        
-                    </Nav>
-                    <Nav className=''>
-                        {
-                            user?  <Nav.Link onClick={handleSignOut} className='text-white' as={Link} to="/login">Sign out</Nav.Link>:<Nav.Link className='text-white' as={Link} to="/login">Login</Nav.Link>
-                           
-                        }
-                    </Nav>
-                </Container>
-            </Navbar>
-        </div>
-    );
-};
+  const handleSignOut = () => {
+    signOut(auth)
+  }
+  return (
+    <div>
+      <Navbar bg='danger' expand='lg' className="nav-bar">
+          <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#link'>Link</Nav.Link>
+            </Nav>
+            <Nav>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
 
-export default Head;
+export default Head
