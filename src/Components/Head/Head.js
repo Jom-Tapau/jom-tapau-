06146a633ac5cccf0314bff708d839edc86d456c
GMShimon <simon.rosedale99@gmail.com>
2022-11-12 19:53:58 +0800

    hover effect added

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import auth from '../../firebase.init'
 import { signOut } from 'firebase/auth'
 import './Head.css'
@@ -21,16 +21,19 @@ const Head = () => {
     <div>
       <Navbar bg='danger' expand='lg' className='nav-bar'>
         <Container>
-        <Navbar.Brand className='text-white' as={Link} to="/home">Jom Tapau</Navbar.Brand>
+        <Nav>
+        <Nav.Link className='text-white nav-link fs-5' as={Link} to="/home">Jom Tapau</Nav.Link>
+        </Nav>
+
         <Navbar.Toggle style={{color:'white'}} aria-controls='basic-navbar-nav' />
         <Navbar.Collapse  id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link className='text-white' as={Link} to="/menu">Menu</Nav.Link>
-            <Nav.Link className='text-white' as={Link} to="/about">About</Nav.Link>
+          <Nav className='me-auto d-flex align-items-center'>
+            <NavLink className='text-white nav-link fs-5' as={Link} to="/menu">Menu</NavLink>
+            <NavLink className='text-white nav-link fs-5' as={Link} to="/about">About</NavLink>
           </Nav>
           <Nav>
-            {user ? (<Nav.Link onClick={handleSignOut} className='text-white' as={Link} to='/login'>Sign out</Nav.Link>) : 
-                (<Nav.Link className='text-white' as={Link} to='/login'>Login</Nav.Link>)
+            {user ? (<Nav.Link onClick={handleSignOut} className='text-white nav-link fs-5' as={Link} to='/login'>Sign out</Nav.Link>) : 
+                (<Nav.Link className=' text-white nav-link fs-5' as={Link} to='/login'>Login</Nav.Link>)
             }
           </Nav>
         </Navbar.Collapse>
