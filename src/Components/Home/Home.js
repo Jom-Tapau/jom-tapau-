1ea631f9ce7600a996132cdc08a128d63149f398
GMShimon <simon.rosedale99@gmail.com>
2022-11-12 21:10:15 +0800

    title on home page added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
 const Home = () => {
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jom Tapau</title>
+      </Helmet>
     return (
         <div>
             <h1>Hello from home</h1>
