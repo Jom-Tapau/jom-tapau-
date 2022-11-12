15e61c931f1e9efc0e2a2baf3571b64481b7cc25
GMShimon <simon.rosedale99@gmail.com>
2022-11-12 16:42:49 +0800

    title of loading component

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Spinner } from 'react-bootstrap'
-
+import Helmet from 'react-helmet';
 const Loading = () => {
   return (
     <div className='mt-5'>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Loading...</title>
+      </Helmet>
       <Spinner animation='border'variant="danger" role='status'>
         <span className='visually-hidden'>Loading...</span>
       </Spinner>
