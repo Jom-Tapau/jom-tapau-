0057e1a0fdf85e496a4b690adda0a271a8a3c93b
GMShimon <simon.rosedale99@gmail.com>
2022-11-14 13:21:33 +0800

    home error fixed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Helmet from 'react-helmet'
 const Home = () => {
   return (
-    <div>
+    <div className='mt-5'>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jom Tapau</title>
+      </Helmet>
       <h1>Hello from home</h1>
     </div>
   )
