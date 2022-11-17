979e300cdeff0aab75a0d41331722a173d40da19
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-17 13:50:02 +0800

    fixed admin wireframing

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -4,42 +4,51 @@ import { Link } from 'react-router-dom';
 
 const Admin = () => {
     return (
-        <div  style={{marginTop:"100px"}}>
-        <h1 className='text-danger mb-5'>What would you like to do today?</h1>
-        
-<div className='d-flex justify-content-lg-center'>
-<div className='me-5'>
-    <Card style={{ width: '180px',  }}>
-      <Card.Img  style={{width:"200px", height:"200px"}} variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" />
-      <Card.Body>
-        
-      
-        <Button as={Link} to="/addfood" variant="danger">Add a Food Item</Button>
-      </Card.Body>
-    </Card>
-    </div>
-    <div className='me-5'>
-    <Card style={{ width: '180px',  }}>
-      <Card.Img style={{width:"200px",height:"200px"}}  variant="top" src="https://m.media-amazon.com/images/I/81hkHb88xoL._SX355_.jpg" />
-      <Card.Body>
-        
-      
-        <Button as={Link} to='/rider' variant="danger">Add a Rider</Button>
-      </Card.Body>
-    </Card>
-    </div>
-    <div className='me-5'>
-    <Card style={{ width: '180px',  }}>
-      <Card.Img style={{width:"200px",height:"200px"}} variant="top" src="https://miro.medium.com/max/512/1*lxnwe1wohMGnGA-qItmpXw.png" />
-      <Card.Body>
-        
-      
-        <Button as={Link} to='/EditUser' variant="danger">Edit User</Button>
-      </Card.Body>
-    </Card>
-    </div>
+<div className='vh-100 '>
+
+<div className='d-flex flex-column bg-danger w-25 ' style={{marginTop:"60px"}}>
+
+  <div className=' bg-danger p-2  text-white   ' >
+<div className='p-2'>
+<img className='w-25 h-25  border border-light rounded-circle ' src="https://w7.pngwing.com/pngs/358/473/png-transparent-computer-icons-user-profile-person-child-heroes-public-relations.png" alt="" />
 </div>
-        </div>
+    <br />
+ <i className='fw-bold'>   Hello! Mr James ,What would you like to do today?</i>
+<br />
+<small>System Admin</small>
+
+  </div>
+  <div className='p-2' style={{backgroundColor:'grey'}}>
+
+<div>
+<Link  to='/addfood' href="" className='text-decoration-none'>
+<div   className='d-flex mt-5 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
+  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
+  
+  </div>
+</Link>
+</div>
+<div>
+<Link  to='/addfood' href="" className='text-decoration-none'>
+<div   className='d-flex mt-1 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
+  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
+  
+  </div>
+</Link>
+</div>
+<div>
+<Link  to='/addfood' href="" className='text-decoration-none'>
+<div   className='d-flex mt-1 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
+  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
+  
+  </div>
+</Link>
+</div>
+
+</div>
+
+</div>
+</div>
     );
 };
 
