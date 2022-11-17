f85f2833f1da1c43a22223bbaeec0ef3f194b3f4
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-11-17 14:11:23 +0800

    admin page design

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,7 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Button, Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
+import { faBowlFood, faCoffee, faMotorcycle, faUserPen } from '@fortawesome/free-solid-svg-icons'
 const Admin = () => {
     return (
 <div className='vh-100 '>
@@ -22,25 +23,31 @@ const Admin = () => {
 
 <div>
 <Link  to='/addfood' href="" className='text-decoration-none'>
-<div   className='d-flex mt-5 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
-  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
-  
+<div   className='d-flex mt-1 justify-content-between'>
+
+
+  <p className='   text-white'>Add a Food Item</p>
+  <FontAwesomeIcon style={{color:'white'}} icon={faBowlFood} />
   </div>
 </Link>
 </div>
 <div>
-<Link  to='/addfood' href="" className='text-decoration-none'>
-<div   className='d-flex mt-1 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
-  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
-  
+<Link  to='/rider' href="" className='text-decoration-none'>
+<div   className='d-flex mt-1 justify-content-between'>
+
+
+  <p className='   text-white'>Add a Rider</p>
+  <FontAwesomeIcon style={{color:'white'}} icon={faMotorcycle} />
   </div>
 </Link>
 </div>
 <div>
-<Link  to='/addfood' href="" className='text-decoration-none'>
-<div   className='d-flex mt-1 border border-white'><img className='w-25' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUK7-fzWVB5Te59sLIGJA4eD2Nrn7hZ5R_kw&usqp=CAU" alt="" />
-  <p className='w-75 mt-3  text-white'>Add a Food Item</p>
-  
+<Link  to='/EditUser' href="" className='text-decoration-none'>
+<div   className='d-flex mt-1 justify-content-between'>
+
+
+  <p className='   text-white'>Edit User</p>
+  <FontAwesomeIcon style={{color:'white'}} icon={faUserPen} />
   </div>
 </Link>
 </div>
