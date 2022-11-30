b3f255e524cc8ce2f25c4b388206ba7ab9d9e90a
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-01 01:31:05 +0800

    added forgot password handler

--- src/Components/ForgotPassword/ForgotPassword.js
+++ src/Components/ForgotPassword/ForgotPassword.js
@@ -1,15 +1,34 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import React from 'react';
+import { useRef } from 'react';
+import auth from '../../firebase.init';
 
 const ForgotPassword = () => {
+    const emailRef= useRef();
+    const handleForgotPassword=()=>{
+const email = emailRef.current.value;
+sendPasswordResetEmail(auth, email)
+  .then(() => {
+    // Password reset email sent!
+    console.log('sent');
+    // ..
+  })
+  .catch((error) => {
+
+    const errorMessage = error.message.substr(9,38);
+    console.log(errorMessage);
+    // ..
+  });
+    }
     return (
         <div  style={{marginTop:"100px"}}>
       <div className='d-flex flex-column justify-content-center align-items-center'>
       <h3 className='text-danger'>Enter the email you used to Register into the system</h3>
       <div className="input-group mb-3 w-25">
-  <input  type="text" class="form-control" placeholder="Enter Your Email" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+  <input ref={emailRef}  type="text" class="form-control" placeholder="Enter Your Email" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
 
 </div>
-<button class="btn btn-danger" type="submit">Submit</button>
+<button onClick={handleForgotPassword} class="btn btn-danger" type="submit">Submit</button>
       </div>
         </div>
     );
