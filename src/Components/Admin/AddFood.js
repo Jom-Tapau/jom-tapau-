bd94d1cc814c746e75b1f4e8f6928e827a6e2403
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-01 05:23:57 +0800

    added fetch data from databse

--- src/Components/Admin/AddFood.js
+++ src/Components/Admin/AddFood.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const AddFood = () => {
+  const nameRef= useRef();
+  const priceRef= useRef();
+  const catRef= useRef();
+  const imgRef= useRef();
+const handleAddFood=(event)=>{
+  
+  event.preventDefault();
+  const name = nameRef.current.value;
+  const price = priceRef.current.value;
+  const imgURL = imgRef.current.value;
+  const category = catRef.current.value;
+ const  user={name, price, imgURL,category};
+  fetch("http://localhost:5000/food",{
+    method: 'POST',
+    headers:{
+      'Content-Type':'application/json',
+    },
+    body:JSON.stringify(user),
+  }).then(
+    res=>res.json()
+  ).then(data=>console.log("success",data));
+}
+
     return (
         <div  style={{marginTop:"100px"}}>
                      <h1 className='text-danger '>Please Add a New Food Item</h1>
@@ -9,21 +33,21 @@ const AddFood = () => {
                <Form className='w-50 mx-auto '>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
    
-        <Form.Control type="text" placeholder="Enter Food Name" />
+        <Form.Control ref={nameRef} type="text" placeholder="Enter Food Name" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     
-        <Form.Control type="text" placeholder="Enter Food Price" />
+        <Form.Control ref={priceRef}  type="text" placeholder="Enter Food Price" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     
-        <Form.Control type="text" placeholder="Paste the Food Image URL" />
+        <Form.Control ref={imgRef}  type="text" placeholder="Paste the Food Image URL" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
     
-    <Form.Control type="text" placeholder="Enter Food Category" />
+    <Form.Control ref={catRef}  type="text" placeholder="Enter Food Category" />
   </Form.Group>
-  <Button className='btn btn-danger'>Submit</Button>
+  <Button onClick={handleAddFood} className='btn btn-danger'>Submit</Button>
     </Form>
         </div>
     );
