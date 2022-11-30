1ee6359fb51a2d80941a358effef65b6f6086a41
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-01 02:27:37 +0800

    Updated admin page

--- src/Components/Admin/AddFood.js
+++ src/Components/Admin/AddFood.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 const AddFood = () => {
     return (
         <div  style={{marginTop:"100px"}}>
-                     <h1 className='text-danger'>Please Add a New Food Item</h1>
-               <Form className='w-50 mx-auto'>
+                     <h1 className='text-danger '>Please Add a New Food Item</h1>
+                     <br />
+               <Form className='w-50 mx-auto '>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+   
+        <Form.Control type="text" placeholder="Enter Food Name" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    
+        <Form.Control type="text" placeholder="Enter Food Price" />
       </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    
+        <Form.Control type="text" placeholder="Paste the Food Image URL" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    
+    <Form.Control type="text" placeholder="Enter Food Category" />
+  </Form.Group>
+  <Button className='btn btn-danger'>Submit</Button>
     </Form>
         </div>
     );
