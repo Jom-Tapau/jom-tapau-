85c0483f2695774d962032270292ab3ccf2e2b38
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-01 08:57:14 +0800

    added animation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import AnimatedNumber from 'react-animated-number/build/AnimatedNumber'
 import Helmet from 'react-helmet'
 const Home = () => {
+
+  const [customerNum, setcustomerNum]=useState(0);
+  useEffect(() => {
+    if(customerNum==1000){}
+    else 
+      setcustomerNum(customerNum+1000)
+  }, []);
   return (
     <div className='mt-5'>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Jom Tapau</title>
       </Helmet>
+      <AnimatedNumber component="text" value={customerNum}
+            style={{
+                fontSize: 200
+            }}
+            frameStyle={perc => (
+               {opacity:perc/100}
+            )}
+            duration={1000}
+            formatValue={n => n.toFixed(0)}/>
       <h1>Hello from home</h1>
+  
     </div>
   )
 }
