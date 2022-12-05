ac941076fa593e4576f3a2a1a422d2100a666ccc
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-05 23:50:26 +0800

    added an icon

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import { faCoffee, faEnvelope, faHouse, faUserPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram,faSnapchat ,faTwitter} from "@fortawesome/free-brands-svg-icons"
+import React from "react";
 import "./Footer.css";
 
-
 const Footer = () => {
-    return (
-        <div>
-            <div>
-            <div class="footer-basic">
-        <footer>
-            <div class="social"><a href="#"><i class="ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+  return (
+    <div>
+      <div>
+        <div class="footer-basic">
+          <footer>
+            <div class="social">
+              <a href="#">
+                <i class="ion-social-instagram">  
+                <FontAwesomeIcon className="fs-1" icon={faFacebook} /></i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-snapchat">   </i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-twitter">   </i>
+              </a>
+              <a href="#">
+                <i class="icon ion-social-facebook"> </i>
+              </a>
+            </div>
             <ul class="list-inline">
-                <li class="list-inline-item"><a href="/home">Home</a></li>
-                <li class="list-inline-item"><a href="/menu">Menu</a></li>
-                <li class="list-inline-item"><a href="/about">About</a></li>
-                <li class="list-inline-item"><a href="#">Contact Us</a></li>
+              <li class="list-inline-item">
+                <a href="/home">Home</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="/menu">Menu</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="/about">About</a>
+      
+            
+         
+             
+          
+ 
+            
+ 
+ 
+
+             
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Contact Us</a>
+              </li>
             </ul>
             <p class="copyright">Jom Tapau © 2022</p>
-        </footer>
-    </div>  
-
+          </footer>
         </div>
-        </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
