3c3cd6f0152202e809954bac23b058a578db1d11
Ishmum <ishmum20000@gmail.com>
2022-12-06 16:34:07 +0600

    Format home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,95 +1,123 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import AnimatedNumber from 'react-animated-number/build/AnimatedNumber'
-import Helmet from 'react-helmet'
-import HomeCarousel from './HomeCarousel'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import AnimatedNumber from "react-animated-number/build/AnimatedNumber";
+import Helmet from "react-helmet";
+import HomeCarousel from "./HomeCarousel";
 
 const Home = () => {
-
-  const [customerNum, setcustomerNum]=useState(0);
-  const [experience, setExperience]=useState(0);
-  const [menu, setMenu]=useState(0);
+  const [customerNum, setcustomerNum] = useState(0);
+  const [experience, setExperience] = useState(0);
+  const [menu, setMenu] = useState(0);
   useEffect(() => {
-    
-      setcustomerNum(1570);
-      setExperience(2);
-      setMenu(23);
+    setcustomerNum(1570);
+    setExperience(2);
+    setMenu(23);
   }, []);
   return (
-    <div className='mt-5'>
+    <div className="mt-5">
       <Helmet>
         <meta charSet="utf-8" />
         <title>Jom Tapau</title>
       </Helmet>
       <HomeCarousel></HomeCarousel>
-<div className='mt-5 d-flex justify-content-around' style={{color:"grey"}}>
-
-<div className=''>
-  <AnimatedNumber component="text" value={experience}
+      <div
+        className="mt-5 d-flex justify-content-around"
+        style={{ color: "grey" }}
+      >
+        <div className="">
+          <AnimatedNumber
+            component="text"
+            value={experience}
             style={{
-                fontSize: 60
+              fontSize: 60,
             }}
-            frameStyle={perc => (
-               {opacity:perc/100}
-            )}
+            frameStyle={(perc) => ({ opacity: perc / 100 })}
             duration={10000}
-            formatValue={n => n.toFixed(0)}/>
-           <p><small>Years of Experience</small></p>
-  </div>
-    <div>
-    <AnimatedNumber component="text" value={menu}
+            formatValue={(n) => n.toFixed(0)}
+          />
+          <p>
+            <small>Years of Experience</small>
+          </p>
+        </div>
+        <div>
+          <AnimatedNumber
+            component="text"
+            value={menu}
             style={{
-                fontSize: 60
+              fontSize: 60,
             }}
-            frameStyle={perc => (
-               {opacity:perc/100}
-            )}
+            frameStyle={(perc) => ({ opacity: perc / 100 })}
             duration={10000}
-            formatValue={n => n.toFixed(0)}/>
-                <p><small>Menus</small></p>
- 
-    </div>
-    <div>
-    <AnimatedNumber component="text" value={customerNum}
+            formatValue={(n) => n.toFixed(0)}
+          />
+          <p>
+            <small>Menus</small>
+          </p>
+        </div>
+        <div>
+          <AnimatedNumber
+            component="text"
+            value={customerNum}
             style={{
-                fontSize: 60
+              fontSize: 60,
             }}
-            frameStyle={perc => (
-               {opacity:perc/100}
-            )}
+            frameStyle={(perc) => ({ opacity: perc / 100 })}
             duration={10000}
-            formatValue={n => n.toFixed(0)}/>
-                <p><small>Satisfied Customers</small></p>
- 
+            formatValue={(n) => n.toFixed(0)}
+          />
+          <p>
+            <small>Satisfied Customers</small>
+          </p>
+        </div>
+      </div>
+
+      <section className="d-flex justify-content-around mt-5">
+        <div className="w-50 ">
+          <img
+            className="w-100 border  rounded"
+            src="https://i.ibb.co/xfKv5cX/IMG-9318.jpg"
+            alt=""
+          />
+        </div>
+        <div className="w-50">
+          <p
+            className="text-danger"
+            style={{
+              fontFamily: "'Ma Shan Zheng', cursive",
+              opacity: "0.5",
+              textAlign: "start",
+              marginLeft: "50px",
+              fontSize: "100px",
+            }}
+          >
+            <i>About</i>
+          </p>
+          <h1 style={{ color: "grey" }}>Jom Tapau</h1>
+          <p
+            style={{
+              textAlign: "start",
+              marginRight: "50px",
+              marginLeft: "50px",
+              color: "grey",
+            }}
+          >
+            JOM TaPAU App is a Pre-order food delivery platform.
+            <br />
+            It Help university students and working adults to afford
+            <br />
+            cheaper food delivery meals and services
+          </p>
+          <i className="fs-3">
+            <p style={{ color: "grey" }}>
+              Sunday to Thursday <b className="text-danger">10am-5pm </b>
+            </p>
+          </i>
+        </div>
+      </section>
+
+      <section></section>
     </div>
-</div>
-  
-<section className='d-flex justify-content-around mt-5' >
-  <div className='w-50 '><img className='w-100 border  rounded' src="https://i.ibb.co/xfKv5cX/IMG-9318.jpg" alt="" /></div>
-  <div className='w-50'>
-    <p className='text-danger' style={{fontFamily:"'Ma Shan Zheng', cursive",opacity:"0.5",textAlign:"start",marginLeft:"50px", fontSize:"100px"}}><i>About</i></p>
-    <h1 style={{color:'grey'}}>Jom Tapau</h1>
-    <p style={{textAlign:"start", marginRight:"50px",marginLeft:"50px",color:"grey"}}>JOM TaPAU App is a
-Pre-order food delivery platform.
-<br />
- It
-Help university students and working adults 
-to afford
-<br />
- cheaper food delivery meals and services
-</p>
-<i className='fs-3'><p style={{color:"grey"}}>Sunday to Thursday <b className='text-danger'>10am-5pm </b></p></i>
-  </div>
-</section>
+  );
+};
 
-<section>
-  
-</section>
-    </div>
-
-    
-  )
-  
-}
-
-export default Home
+export default Home;
