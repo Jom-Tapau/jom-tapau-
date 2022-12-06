6dd43122a95325dd185ac0cdf8824632b0298dbb
Ishmum <ishmum20000@gmail.com>
2022-12-06 21:31:26 +0600

    Format doc

--- src/Components/Rider/Rider.js
+++ src/Components/Rider/Rider.js
@@ -1,78 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
-import { UNSAFE_DataRouterContext, useNavigate } from 'react-router-dom';
-import AddRider from './AddRider';
-import AllRider from './AllRider';
-import ApplicantRider from './ApplicantRider';
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import { UNSAFE_DataRouterContext, useNavigate } from "react-router-dom";
+import AddRider from "./AddRider";
+import AllRider from "./AllRider";
+import ApplicantRider from "./ApplicantRider";
 
 const Rider = () => {
-    const [users,setUser] = useState([]);
-    const [applicants,setApplicants] = useState([]);
-    const [notRider,setNotRider] = useState([]);
-    const [allRiders,setRiders] = useState([]);
-    const [addRider,setAddRider] = useState(false)
-    const [appBtn,setAppBtn] = useState(false)
+  const [users, setUser] = useState([]);
+  const [applicants, setApplicants] = useState([]);
+  const [notRider, setNotRider] = useState([]);
+  const [allRiders, setRiders] = useState([]);
+  const [addRider, setAddRider] = useState(false);
+  const [appBtn, setAppBtn] = useState(false);
 
-    const navigate = useNavigate()
-    useEffect(()=>{
-        fetch('http://localhost:5000/user')
-        .then(res=>res.json())
-        .then(data=>{
-            setUser(data);
-            let applicantRider=[];
-            // filter the rider from the user data
-            let riderUser=[]
-            let remainning=[]
-            data.map(user=>{
-                if(user.rider==true){
-                    riderUser.push(user);
-                }
-                else if(user.rider=="rejected"){
-                    
-                }
-                else{
-                remainning.push(user);}
+  const navigate = useNavigate();
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+        let applicantRider = [];
+        // filter the rider from the user data
+        let riderUser = [];
+        let remainning = [];
+        data.map((user) => {
+          if (user.rider == true) {
+            riderUser.push(user);
+          } else if (user.rider == "rejected") {
+          } else {
+            remainning.push(user);
+          }
 
-                if(user.rider!=true){
-                    applicantRider.push(user)
-                }
-            })
-            setRiders(riderUser)
-            setNotRider(remainning)
-            setApplicants(applicantRider)
-            
-        })
-    },[])
+          if (user.rider != true) {
+            applicantRider.push(user);
+          }
+        });
+        setRiders(riderUser);
+        setNotRider(remainning);
+        setApplicants(applicantRider);
+      });
+  }, []);
 
-    const handleAddRider = () =>{
-        setAddRider(!addRider);
-        setAppBtn(false);
-    }
-    const handleApplicants=()=>{
-        setAppBtn(!appBtn);
-        setAddRider(false)
-    }
-    return (
-        <div>
-            <Helmet>
-                <title>Rider</title>
-            </Helmet>
-            <div style={{marginTop:"80px"}}>
-            <Button onClick={handleAddRider} className="me-4" variant={addRider?'danger':'outline-danger'}>Add Rider</Button>
-            <Button onClick={handleApplicants}variant={appBtn?'danger':'outline-danger'}>Applicants</Button>
-            </div>
-            {
-                !addRider&&!appBtn?<AllRider rider={allRiders} ></AllRider>:""
-            }
-            {
-                addRider&&<AddRider notRider={notRider}></AddRider>
-            }
-            {
-                appBtn&&<ApplicantRider applicants={applicants}></ApplicantRider>
-            }
-        </div>
-    );
+  const handleAddRider = () => {
+    setAddRider(!addRider);
+    setAppBtn(false);
+  };
+  const handleApplicants = () => {
+    setAppBtn(!appBtn);
+    setAddRider(false);
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>Rider</title>
+      </Helmet>
+      <div style={{ marginTop: "80px" }}>
+        <Button
+          onClick={handleAddRider}
+          className="me-4"
+          variant={addRider ? "danger" : "outline-danger"}
+        >
+          Add Rider
+        </Button>
+        <Button
+          onClick={handleApplicants}
+          variant={appBtn ? "danger" : "outline-danger"}
+        >
+          Applicants
+        </Button>
+      </div>
+      {!addRider && !appBtn ? <AllRider rider={allRiders}></AllRider> : ""}
+      {addRider && <AddRider notRider={notRider}></AddRider>}
+      {appBtn && <ApplicantRider applicants={applicants}></ApplicantRider>}
+    </div>
+  );
 };
 
 export default Rider;
