bc6a508efa0241018bfc3bcfb56dc51dc07331c3
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-12-15 11:04:49 +0800

    implement search

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Nav } from 'react-bootstrap';
+import Item from '../Item/Item';
 import "./Categories.css";
 
 const Categories = () => {
+    const [foods,setFood]= useState([]);
+
+    const clickDrinks=()=>{
+        foods.filter(food=>food.category=="Drinks"?  <div className='col-lg-4 col-md-6 col-sm-12'>
+        <Item food={food}></Item>
+      </div>:<></>)
+    }
     return (
         <div >
             <Nav defaultActiveKey="#popular" className="flex-column categories">
@@ -10,7 +18,7 @@ const Categories = () => {
                 <Nav.Link className='link' eventKey="link-1">Set Meals</Nav.Link>
                 <Nav.Link className='link' eventKey="link-2">BreakFast</Nav.Link>
                 <Nav.Link className='link' eventKey="disabled">Lunch</Nav.Link>
-                <Nav.Link className='link' eventKey="disabled">Drinks</Nav.Link>
+                <Nav.Link onClick={clickDrinks} className='link'>Drinks</Nav.Link>
             </Nav>
         </div>
     );
