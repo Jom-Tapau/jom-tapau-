985d189f3741c6bd3764ef5b29c0a65e8f83cada
Ishmum <ishmum20000@gmail.com>
2022-12-15 15:18:07 +0800

    Remove about section and add component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -71,52 +71,6 @@ const Home = () => {
           </p>
         </div>
       </div>
-
-      <section className="d-flex justify-content-around mt-5">
-        <div className="w-50 ">
-          <img
-            className="w-100 border  rounded"
-            src="https://i.ibb.co/xfKv5cX/IMG-9318.jpg"
-            alt=""
-          />
-        </div>
-        <div className="w-50">
-          <p
-            className="text-danger"
-            style={{
-              fontFamily: "'Ma Shan Zheng', cursive",
-              opacity: "0.5",
-              textAlign: "start",
-              marginLeft: "50px",
-              fontSize: "100px",
-            }}
-          >
-            <i>About</i>
-          </p>
-          <h1 style={{ color: "grey" }}>Jom Tapau</h1>
-          <p
-            style={{
-              textAlign: "start",
-              marginRight: "50px",
-              marginLeft: "50px",
-              color: "grey",
-            }}
-          >
-            JOM TaPAU App is a Pre-order food delivery platform.
-            <br />
-            It Help university students and working adults to afford
-            <br />
-            cheaper food delivery meals and services
-          </p>
-          <i className="fs-3">
-            <p style={{ color: "grey" }}>
-              Sunday to Thursday <b className="text-danger">10am-5pm </b>
-            </p>
-          </i>
-        </div>
-      </section>
-
-      <section></section>
       <Description></Description>
     </div>
 
