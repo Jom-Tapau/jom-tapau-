952a220e382792dd928a89e746036092c5ea5688
GMShimon <simon.rosedale99@gmail.com>
2022-12-15 15:43:49 +0800

    food card

--- src/Components/EditFood/SingleFood.js
+++ src/Components/EditFood/SingleFood.js
@@ -1,12 +1,26 @@
-import React from 'react';
-
+import React from "react";
+import  './SingleFood.css';
 const singleFood = () => {
-    // console.log(props.food)
-    return (
-        <div className='mt-5'>
-            <h1>Hello Single Food</h1>
+  // console.log(props.food)
+  return (
+    <div className="mt-5">
+      <div className="card" style={{width: "18rem"}}>
+        <div className="d-flex justify-content-center">
+        <img src="https://i.ibb.co/SRJYQQv/IMG-9330.jpg" className="card-img-top food-img" alt="..." />
         </div>
-    );
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default singleFood;
