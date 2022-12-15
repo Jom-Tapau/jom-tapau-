7c49f91f5601451cd37016ecbe10a815f645aa34
GMShimon <simon.rosedale99@gmail.com>
2022-12-15 11:41:59 +0800

    search bar added

--- src/Components/EditFood/EditFood.js
+++ src/Components/EditFood/EditFood.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import "./EditFood.css";
 const EditFood = () => {
-    return (
-        <div>
-            
+  return (
+    <section>
+      <div style={{ marginTop: "100px" }}>
+        <div className="wrap">
+          <div className="search">
+            <input
+              type="text"
+              className="searchTerm"
+              placeholder="What are you looking for?"
+            />
+            <button type="submit" class="searchButton">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default EditFood;
