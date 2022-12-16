eac7eb24d12d1a23cef84546d0a339057de23737
Ishmum <ishmum20000@gmail.com>
2022-12-16 21:06:24 +0800

    Add item name section

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./CartItem.css";
 
-const CartItem = (porps) => {
-    const { name, description, price, imgURL } = props.food;
-    
-    return (
-        <div>
-            
-        </div>
-    );
+const CartItem = (props) => {
+  const { name, description, price, imgSrc } = props.food;
+
+  return (
+    <div className="cart-item">
+      <div className="item-name">
+        <img src={imgSrc} />
+        <h4>{name}</h4>
+      </div>
+      
+    </div>
+  );
 };
 
 export default CartItem;
