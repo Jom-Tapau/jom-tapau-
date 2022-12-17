e4db1a46b49aebc3dfaa1d65b53cd0fdd9568ab8
GMShimon <simon.rosedale99@gmail.com>
2022-12-18 05:22:11 +0800

    food card designing-1

--- src/Components/EditFood/SingleFood.js
+++ src/Components/EditFood/SingleFood.js
@@ -1,18 +1,33 @@
 import React from "react";
-import  './SingleFood.css';
+import "./SingleFood.css";
 const singleFood = () => {
   // console.log(props.food)
   return (
     <div className="mt-5">
-      <div className="card" style={{width: "18rem"}}>
+      <div className="card" style={{ width: "18rem" }}>
         <div className="d-flex justify-content-center">
-        <img src="https://i.ibb.co/SRJYQQv/IMG-9330.jpg" className="card-img-top food-img" alt="..." />
+          <img
+            src="https://i.ibb.co/SRJYQQv/IMG-9330.jpg"
+            className="card-img-top food-img"
+            alt="..."
+          />
         </div>
         <div className="card-body">
           <h5 className="card-title">Nasi Goreng Ayam</h5>
-          <p className="card-text fst-italic">
-            Breakfast set meal
-          </p>
+          <div className="d-flex justify-content-center">
+            <p 
+              style={{ width: "120px" }}
+              className="card-text fst-italic border border-3 border-light rounded-pill"
+            >
+              Breakfast
+            </p>
+            <p
+              style={{ width: "110px" }}
+              className="card-text fst-italic border border-3 border-light rounded-pill"
+            >
+              Breakfast
+            </p>
+          </div>
           <a href="#" className="btn btn-primary">
             Go somewhere
           </a>
