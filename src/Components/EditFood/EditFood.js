59a712daaed1137bd3677e53d342f6c47cb3ffe5
GMShimon <simon.rosedale99@gmail.com>
2022-12-19 15:37:33 +0800

    food mapping-2

--- src/Components/EditFood/EditFood.js
+++ src/Components/EditFood/EditFood.js
@@ -1,34 +1,30 @@
 import React, { useRef } from "react";
 import { useState } from "react";
 import useFood from "../../hooks/useFood";
+import SingleFood from "./SingleFood";
 import "./EditFood.css";
-import SingleFood from './SingleFood'
 import { Helmet } from "react-helmet";
 const EditFood = () => {
+  const [inputValue, setInputValue] = useState("");
+  const inputField = useRef("");
 
-    const [inputValue,setInputValue] = useState('');
-    const inputField = useRef('');
-    
+  const { foods, setFood } = useFood();
+  const handleSearchFood = () => {
+    setInputValue(inputField.current.value);
+    console.log(foods);
 
-    const {foods,setFood} = useFood();
-    console.log(foods)
-    const handleSearchFood = () =>{
-        setInputValue(inputField.current.value)
-        console.log(foods);
-
-        fetch('',{
-          method:'POST',
-          headers:{
-            'content-type':'application/json'
-          },
-          body: JSON.stringify(inputValue)
-        })
-        .then(res=>res.json())
-        .then(data=>{
-          if(data.length>0)
-          setFood(data)
-        })
-    }
+    fetch("", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(inputValue),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) setFood(data);
+      });
+  };
 
   return (
     <section className="bg-light">
@@ -41,27 +37,31 @@ const EditFood = () => {
         <div className="wrap ">
           <div className="search mb-4">
             <input
-                ref={inputField}
+              ref={inputField}
               type="text"
               className="searchTerm"
               placeholder="What are you looking for?"
             />
-            <button onClick={handleSearchFood} type="submit" class="searchButton">
+            <button
+              onClick={handleSearchFood}
+              type="submit"
+              class="searchButton"
+            >
               <i className="fa fa-search"></i>
             </button>
           </div>
         </div>
       </div>
-      {/* end of search bar code  */}  
-      
+      {/* end of search bar code  */}
+
       {/* send single food to singleFood component */}
-      <div className="container">
-      <section className="row justify-content-center">
-      {
-        foods.map(food=><SingleFood key={food._id} food={food}></SingleFood>)
-      }
-      </section>
-      </div>
+        <div className="container">
+          <section className="row">
+            {foods.map((food) => (
+              <SingleFood key={food._id} food={food}></SingleFood>
+            ))}
+          </section>
+        </div>
     </section>
   );
 };
