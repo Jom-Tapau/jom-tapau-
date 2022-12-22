1f574da05a254f26ae7cd4834d8f837605f994a3
GMShimon <simon.rosedale99@gmail.com>
2022-12-22 15:11:54 +0800

    delete food

--- src/Components/EditFood/SingleFood.js
+++ src/Components/EditFood/SingleFood.js
@@ -1,39 +1,41 @@
 import React from "react";
 import "./SingleFood.css";
-const singleFood = () => {
-  // const {_id,name,price,imgURL,category} = props.food;
+const singleFood = (props) => {
+  const {_id,name,price,imgURL,category} = props.food;
   // console.log(props.food)
 
   const handleFoodEdit = id =>{
     console.log(id)
   }
   const handleFoodDelete = id =>{
-    console.log(id)
+    fetch(`http://localhost:5000/foodDelete/${id}`)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
   }
   return (
     <div className="mt-5 col-4">
       <div className="card food-card mt-5" style={{ width: "18rem" }}>
         <div className="d-flex justify-content-center">
           <img
-            src="https://i.ibb.co/SRJYQQv/IMG-9330.jpg"
+            src={imgURL}
             className="card-img-top food-img"
             alt="..."
           />
         </div>
         <div className="card-body">
-          <h5 className="card-title">Nasi Goreng Ayam</h5>
+          <h5 className="card-title">{name}</h5>
             <div className="d-flex justify-content-center mb-3">
             <p 
               style={{ width: "120px" }}
               className="category-font card-text fst-italic border border-3 border-light rounded-pill"
             >
-              Breakfast
+              {category}
             </p>
             </div>
-          <span onClick={()=>handleFoodEdit(1)} class="material-symbols-sharp me-5 edit-btn">
+          <span onClick={()=>handleFoodEdit(_id)} class="material-symbols-sharp me-5 edit-btn">
             edit
           </span>
-          <span onClick={()=>handleFoodDelete(2)} class="material-symbols-sharp delete-btn">
+          <span onClick={()=>handleFoodDelete(_id)} class="material-symbols-sharp delete-btn">
             delete
           </span>
         </div>
