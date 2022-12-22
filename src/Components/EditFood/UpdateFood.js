9caf2b93a85576d633cced5c59bfd72a549ba644
GMShimon <simon.rosedale99@gmail.com>
2022-12-23 05:05:53 +0800

    get food by id

--- src/Components/EditFood/UpdateFood.js
+++ src/Components/EditFood/UpdateFood.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateFood = () => {
     const {foodID}  = useParams();
+    useEffect(()=>{
+        fetch(`http://localhost:5000/food/${foodID}`)
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div style={{ paddingTop: "80px" }}>
             <h1>hello</h1>
