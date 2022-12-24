e894551c35c8334d7f00fb79a6d27e49c663de11
GMShimon <simon.rosedale99@gmail.com>
2022-12-24 19:35:06 +0800

    update form created

--- src/Components/EditFood/UpdateFood.js
+++ src/Components/EditFood/UpdateFood.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import './UpdateFood.css';
 const UpdateFood = () => {
   const [food, singleFood] = useState({});
   const { foodID } = useParams();
@@ -36,46 +36,24 @@ const UpdateFood = () => {
             <form action="" method="post">
                 <table>
                     <tr>
-                        <td className="profile">Name:</td>
+                        <td  className="profile">Food Name:</td>
                         <td className="profile"> <input style={{width:"110%"}} type="text" id="updateName" name="updateName" value="<?php echo $fullname?>"/></td>
                     </tr>
                     <tr>
-                        <td className="profile">Gender:</td>
-                        <td className="profile"> 
-                        <select id="updateGender" style={{width: "114%"}} name="updateGender">
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                        </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="profile">Father Name:</td>
+                        <td className="profile">Price:</td>
                         <td className="profile"> <input style={{width: "110%"}} type="text" id="updateFatherName" name="updateFatherName" value="<?php echo $fatherName?>"/></td>
                     </tr>
                     <tr>
-                        <td className="profile">Mother Name:</td>
+                        <td className="profile">URL:</td>
                         <td className="profile"> <input style={{width: "110%"}} type="text" id="updateMotherName" name="updateMotherName" value="<?php echo $motherName?>"/></td>
                     </tr>
                     <tr>
-                        <td className="profile">Date of Birth:</td>
-                        <td className="profile"> <input style={{width: "110%"}} type="date" placeholder="dd-mm-yyyy"  id="updateDOB" name="updateDOB" value="<?php echo $dob?>"/></td>
-                    </tr>
-                    <tr>
-                        <td className="profile">Email:</td>
+                        <td className="profile">Category:</td>
                         <td className="profile"> <input style={{width: "110%"}} type="text" id="updateEmail" name="updateEmail" value="<?php echo $email?>"/></td>
-                    </tr>
-                    <tr>
-                        <td className="profile">Role:</td>
-                        <td className="profile"> <input style={{width: "110%"}} disabled type="text" id="updateRole" name="updateRole" value="<?php echo $role?>"/></td>
-                    </tr>
-                   
-                    <tr>
-                        <td className="profile">Phone Number:</td>
-                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updatePhone" name="updatePhone" value="<?php echo $phone?>"/></td>
                     </tr>
                 </table>
                 <div style={{textAlign:"center"}}>
-                    <button type="submit" id="submit-btn" onClick="submit()" name="update">Submit</button>
+                    <button type="submit" id="submit-btn" name="update">Update</button>
                 </div>
             </form>
             
