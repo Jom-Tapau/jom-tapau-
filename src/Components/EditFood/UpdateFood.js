96fbafc1a468a3d15b3fd82cd7354f212c1d7b4f
GMShimon <simon.rosedale99@gmail.com>
2022-12-25 23:08:49 +0800

    get food details

--- src/Components/EditFood/UpdateFood.js
+++ src/Components/EditFood/UpdateFood.js
@@ -10,6 +10,7 @@ const UpdateFood = () => {
       .then((res) => res.json())
       .then((data) => singleFood(data));
   }, []);
+  console.log(food);
   /* 
         now using direct url of image to update the picture of the food
         later we will upload only the image to update the picture
@@ -30,26 +31,26 @@ const UpdateFood = () => {
         <section id="edit-profile-container" className="edit-profile-container">
         <div className="box">
             <div>
-                <h2>Edit profile</h2>
+                <h2>Food Details</h2>
             </div>
             <hr/>
             <form action="" method="post">
                 <table>
                     <tr>
                         <td  className="profile">Food Name:</td>
-                        <td className="profile"> <input style={{width:"110%"}} type="text" id="updateName" name="updateName" value="<?php echo $fullname?>"/></td>
+                        <td className="profile"> <input style={{width:"110%"}} type="text" id="updateName" name="updateName" value={food.name}/></td>
                     </tr>
                     <tr>
                         <td className="profile">Price:</td>
-                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updateFatherName" name="updateFatherName" value="<?php echo $fatherName?>"/></td>
+                        <td className="profile"><input style={{width: "110%"}} type="text" id="updateFatherName" name="updateFatherName" value={food.price}/></td>
                     </tr>
                     <tr>
                         <td className="profile">URL:</td>
-                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updateMotherName" name="updateMotherName" value="<?php echo $motherName?>"/></td>
+                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updateMotherName" name="updateMotherName" value={food.imgURL}/></td>
                     </tr>
                     <tr>
                         <td className="profile">Category:</td>
-                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updateEmail" name="updateEmail" value="<?php echo $email?>"/></td>
+                        <td className="profile"> <input style={{width: "110%"}} type="text" id="updateEmail" name="updateEmail" value={food.category}/></td>
                     </tr>
                 </table>
                 <div style={{textAlign:"center"}}>
