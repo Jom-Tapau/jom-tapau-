2c6da818b6c1ed1a7d71769366d46fd2968827a7
Ishmum <ishmum20000@gmail.com>
2022-12-31 18:34:23 +0800

    Add user image and name

--- src/Components/RiderDash/Order.js
+++ src/Components/RiderDash/Order.js
@@ -7,7 +7,8 @@ const Order = () => {
     return (
         <div className='order'>
             <div className='name-user'>
-                
+                <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' />
+                <h4>Name</h4>
             </div>
 
             <div {...getCollapseProps()}>
