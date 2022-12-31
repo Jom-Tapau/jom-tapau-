8fba080d0509da920d3b4f5731c44ee92ca84ea6
Ishmum <ishmum20000@gmail.com>
2022-12-31 20:19:15 +0800

    Add address and total section

--- src/Components/RiderDash/Order.js
+++ src/Components/RiderDash/Order.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import useCollapse from 'react-collapsed';
+import React from "react";
+import useCollapse from "react-collapsed";
+import "./Order.css";
 
 const Order = () => {
     const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
     return (
-        <div className='order'>
-            <div className='name-user'>
-                <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' />
-                <h4>Name</h4>
+        <div className="order">
+            <div className="info-container">
+                <div className="name-user">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" />
+                    <h4>Name</h4>
+                </div>
+                <div className="address">
+                    KLG Campus Resicence, Taman Sri Pulai.
+                </div>
+                <div className="total">
+                    RM 29.50
+                </div>
             </div>
 
-            <div {...getCollapseProps()}>
-                
-            </div>
+            <div className="items" {...getCollapseProps()}></div>
 
-            <div className="header" {...getToggleProps()}>
-                {isExpanded ? "Collapse" : "Expand"}
+            <div className="toggle" {...getToggleProps()}>
+                {isExpanded ? "See less" : "See more"}
             </div>
         </div>
     );
