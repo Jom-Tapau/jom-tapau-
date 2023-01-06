dbf421d54b103f7550cdcaed72968c824e3ff773
GMShimon <simon.rosedale99@gmail.com>
2023-01-06 23:09:07 +0800

    fetch all orders

--- src/Components/RiderDash/RiderDash.js
+++ src/Components/RiderDash/RiderDash.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Order from "./Order";
 import './RiderDash.css';
 
 const RiderDash = () => {
+
+    const [orders,setOrders] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/allOrders')
+        .then(res=>res.json())
+        .then(data=>setOrders(data))
+    },[])
+
+    console.log(orders)
+
     return (
         <div className="rider-dash">
             <Order />
