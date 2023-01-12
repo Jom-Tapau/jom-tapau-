55a7a486999a31deaff66e334603099c84804fb2
adnanshafi99 <adnanshafi@graduate.utm.my>
2023-01-12 15:02:56 +0800

    Update editprofile

---
+++ src/Components/ProfilePage/EditProfilePage.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./EditProfile.css";
+
+const EditProfilePage = () => {
+    const navigate = useNavigate();
+    const submit = () => {
+        navigate("/profile");
+    }
+
+    return (
+        <div>
+        <div className='editprofile-container mb-3'>
+            <section id="edit-container" class="edit-container">
+                <div class="">
+                    <div>
+                        <h2 >Edit profile</h2>
+                    </div>
+                    <hr />
+                        <form action="" method="post">
+                            <table>
+                                <tr>
+                                    <td class="profile">Name:</td>
+                                    <td class="profile"> <input disabled style={{width:'110%'}} type="text" id="updateName" name="updateName" value="" /></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Gender:</td>
+                                    <td class="profile">
+                                        <select id="updateGender" style={{width: '114%'}} name="updateGender">
+                                            <option value="Male">Male</option>
+                                            <option value="Female">Female</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Father Name:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updateFatherName" name="updateFatherName" value=""/></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Mother Name:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updateMotherName" name="updateMotherName" value=""/></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Date of Birth:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} type="date" placeholder="dd-mm-yyyy" id="updateDOB" name="updateDOB" value=""/></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Email:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updateEmail" name="updateEmail" value=""/></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Role:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} disabled type="text" id="updateRole" name="updateRole" value=""/></td>
+                                </tr>
+                                <tr>
+                                    <td class="profile">Phone Number:</td>
+                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updatePhone" name="updatePhone" value=""/></td>
+                                </tr>
+                            </table>
+                            <div style={{textAlign:'center'}}>
+                                <button type="submit" id="submit-btn" name="update">Submit</button>
+                            </div>
+                        </form>
+
+                </div>
+            </section>
+        </div>
+        </div>
+    );
+};
+
+export default EditProfilePage;
