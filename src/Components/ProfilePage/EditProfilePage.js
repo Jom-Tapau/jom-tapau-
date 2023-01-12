b9d4e06821558d7d6b014f1f36478df49353f923
adnanshafi99 <adnanshafi@graduate.utm.my>
2023-01-12 15:11:50 +0800

    Updating editprofile

--- src/Components/ProfilePage/EditProfilePage.js
+++ src/Components/ProfilePage/EditProfilePage.js
@@ -33,14 +33,6 @@ const EditProfilePage = () => {
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td class="profile">Father Name:</td>
-                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updateFatherName" name="updateFatherName" value=""/></td>
-                                </tr>
-                                <tr>
-                                    <td class="profile">Mother Name:</td>
-                                    <td class="profile"> <input style={{width: '110%'}} type="text" id="updateMotherName" name="updateMotherName" value=""/></td>
-                                </tr>
-                                <tr>
                                     <td class="profile">Date of Birth:</td>
                                     <td class="profile"> <input style={{width: '110%'}} type="date" placeholder="dd-mm-yyyy" id="updateDOB" name="updateDOB" value=""/></td>
                                 </tr>
