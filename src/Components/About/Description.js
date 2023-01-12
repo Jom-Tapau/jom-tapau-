eeea806e53b251124c4636ade69204f4ab0b0fbd
adnanshafi99 <adnanshafi@graduate.utm.my>
2023-01-12 15:17:18 +0800

    Centering About page description

--- src/Components/About/Description.js
+++ src/Components/About/Description.js
@@ -7,7 +7,7 @@ const Description = () => {
                 <div className='w-50 '><img className='w-100 border  rounded' src="https://i.ibb.co/xfKv5cX/IMG-9318.jpg" alt="" /></div>
                 <div className='w-50'>
                     <p className='text-danger' style={{ fontFamily: "'Ma Shan Zheng', cursive", opacity: "0.5", textAlign: "center", marginLeft: "50px", fontSize: "100px" }}><i>About</i></p>
-                    <h1 style={{ color: 'grey' }}>Jom Tapau</h1>
+                    <h1 style={{ textAlign: "center", color: 'grey' }}>Jom Tapau</h1>
                     <p style={{ textAlign: "center", marginRight: "50px", marginLeft: "50px", color: "grey" }}>
                         JOM TaPAU App is a
                         Pre-order food delivery platform.
@@ -16,7 +16,7 @@ const Description = () => {
                         <br />
                         cheaper food delivery meals and services
                     </p>
-                    <i className='fs-3'><p style={{ color: "grey" }}>Sunday to Thursday <b className='text-danger'>10am-5pm </b></p></i>
+                    <i className='fs-3'><p style={{textAlign: "center", color: "grey" }}>Sunday to Thursday <b className='text-danger'>10am-5pm </b></p></i>
                 </div>
             </section>
         </div>
