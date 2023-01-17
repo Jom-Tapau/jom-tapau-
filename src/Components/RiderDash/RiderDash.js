a22a17595124fb8e94e60414ed9e3a40874b73fc
GMShimon <simon.rosedale99@gmail.com>
2023-01-17 13:40:01 +0800

    create two button

--- src/Components/RiderDash/RiderDash.js
+++ src/Components/RiderDash/RiderDash.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Order from "./Order";
-import './RiderDash.css';
+import "./RiderDash.css";
 import useOrders from "../../hooks/useOrders";
 import { Helmet } from "react-helmet";
+import { Button } from "react-bootstrap";
 
 const RiderDash = () => {
-    const {orders,otherOrder} = useOrders();
+  const { orders, otherOrder } = useOrders();
+  const [addRider, setAddRider] = useState(false);
+  const [appBtn, setAppBtn] = useState(false);
+  const handleAddRider = () => {
+    setAddRider(!addRider);
+    setAppBtn(false);
+  };
+  const handleApplicants = () => {
+    setAppBtn(!appBtn);
+    setAddRider(false);
+  };
 
-    return (
-        <div className="rider-dash">
-            <Helmet>
-                <title>RiderDashboard</title>
-            </Helmet>
-            {
-                otherOrder?.map(order=><Order
-                key={order._id}
-                order={order}
-                ></Order>)
-            }
-        </div>
-    );
+  return (
+    <div className="rider-dash">
+      <Helmet>
+        <title>RiderDashboard</title>
+      </Helmet>
+      <div className="mb-4">
+        <Button
+          onClick={handleAddRider}
+          className="me-4"
+          variant={addRider ? "danger" : "outline-danger"}
+          size={"sm"}
+        >
+          Add Rider
+        </Button>
+
+        <Button
+          onClick={handleApplicants}
+          variant={appBtn ? "danger" : "outline-danger"}
+          size={"sm"}
+        >
+          Applicants
+        </Button>
+      </div>
+
+      {otherOrder?.map((order) => (
+        <Order key={order._id} order={order}></Order>
+      ))}
+    </div>
+  );
 };
 
 export default RiderDash;
